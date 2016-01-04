14250c889a295cee60112366284a7e5dea21cadd
cmotc <cmotc@openmailbox.org>
2016-01-04 03:01:40 -0500

    removed hyphenate

---
+++ debian.sh
@@ -0,0 +1,52 @@
+#! /bin/sh
+# Configure your paths and filenames
+SOURCEBINPATH=.
+SOURCEDOC=README.md
+SRCFOLDER=vaLAIR-data
+DEBFOLDER=valair-data
+DEBVERSION=$(date +%Y%m%d)
+
+cd $SRCFOLDER
+
+git pull origin master
+
+DEBFOLDERNAME="../$DEBFOLDER-$DEBVERSION"
+
+# Create your scripts source dir
+mkdir $DEBFOLDERNAME
+
+# Copy your script to the source dir
+cp $SOURCEBINPATH $DEBFOLDERNAME -R
+cd $DEBFOLDERNAME
+
+# Create the packaging skeleton (debian/*)
+dh_make -s --indep --createorig 
+
+mkdir -p debian/tmp
+cp -R bin share debian/tmp
+
+# Remove make calls
+grep -v makefile debian/rules > debian/rules.new 
+mv debian/rules.new debian/rules 
+
+# debian/install must contain the list of scripts to install 
+# as well as the target directory
+echo $SOURCEDOC usr/share/doc/$DEBFOLDER >> debian/install
+for d in share/lair/*; do
+    if [ -d $d ]; then
+        for e in $d/*; do
+            echo $e >> debian/install
+        done
+    else
+        echo $d >> debian/install
+    fi
+done
+
+# Remove the example files
+rm debian/*.ex
+
+dpkg-source --commit
+
+# Build the package.
+# You  will get a lot of warnings and ../somescripts_0.1-1_i386.deb
+debuild -us -uc > ../log 
