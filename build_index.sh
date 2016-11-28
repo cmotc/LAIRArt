61b47da9ccd445acae27604d7e90aae3f2d626ab
idk <idk@fireaxe.local>
2016-11-28 05:07:10 -0500

    added test sounds and modified build_indexes script

--- build_index.sh
+++ build_index.sh
@@ -1,9 +1,19 @@
 #! /bin/sh
 FOLDERS=$(find share/lair/* -type d)
 rm share/lair/images.list
+rm share/lair/sounds.list
 for FOLDER in $FOLDERS; do
 	CATEGORY=$(echo $FOLDER | sed 's|share/lair/||')
-	for FILE in $(find $FOLDER -name *.png ); do
-		echo $FILE $CATEGORY default >> share/lair/images.list
+	for FILE in $(find $FOLDER -name *.png); do
+		FPATH=$(echo $FILE | sed 's|share/lair|/usr/share/lair|')
+		echo $FPATH $CATEGORY default >> share/lair/images.list
+	done
+	for FILE in $(find $FOLDER -name *.wav); do
+		FPATH=$(echo $FILE | sed 's|share/lair|/usr/share/lair|')
+		echo $FPATH $CATEGORY default >> share/lair/sounds.list
+	done
+	for FILE in $(find $FOLDER -name *.ttf); do
+		FPATH=$(echo $FILE | sed 's|share/lair|/usr/share/lair|')
+		echo $FPATH $CATEGORY default >> share/lair/fonts.list
 	done
 done
---
+++ copy.sh
@@ -0,0 +1,2 @@
+#! /bin/sh
+sudo rm -rf /usr/share/lair && sudo cp -Rv share/lair /usr/share/lair
